93ad5b2f2a88975dcaf8574f29ad4a6c7acea39c
Timtim1978 <timtimmulyani.aisy@gmail.com>
2025-10-11 00:37:16 +0700

    Fix: Add name and price to products in FeaturedProducts

--- src/components/featured/FeaturedProducts.tsx
+++ src/components/featured/FeaturedProducts.tsx
@@ -4,14 +4,54 @@
 import ProductCard from '../ProductCard';
 
 const products = [
-  { id: '1', image: '/images/product-card1.svg' },
-  { id: '2', image: '/images/product-card2.svg' },
-  { id: '3', image: '/images/product-card3.svg' },
-  { id: '4', image: '/images/product-card4.svg' },
-  { id: '5', image: '/images/product-card5.svg' },
-  { id: '6', image: '/images/product-card6.svg' },
-  { id: '7', image: '/images/product-card7.svg' },
-  { id: '8', image: '/images/product-card8.svg' },
+  {
+    id: '1',
+    image: '/images/product-card1.svg',
+    name: 'Product 1',
+    price: 100,
+  },
+  {
+    id: '2',
+    image: '/images/product-card2.svg',
+    name: 'Product 2',
+    price: 150,
+  },
+  {
+    id: '3',
+    image: '/images/product-card3.svg',
+    name: 'Product 3',
+    price: 200,
+  },
+  {
+    id: '4',
+    image: '/images/product-card4.svg',
+    name: 'Product 4',
+    price: 250,
+  },
+  {
+    id: '5',
+    image: '/images/product-card5.svg',
+    name: 'Product 5',
+    price: 300,
+  },
+  {
+    id: '6',
+    image: '/images/product-card6.svg',
+    name: 'Product 6',
+    price: 350,
+  },
+  {
+    id: '7',
+    image: '/images/product-card7.svg',
+    name: 'Product 7',
+    price: 400,
+  },
+  {
+    id: '8',
+    image: '/images/product-card8.svg',
+    name: 'Product 8',
+    price: 450,
+  },
 ];
 
 export default function FeaturedProducts() {
